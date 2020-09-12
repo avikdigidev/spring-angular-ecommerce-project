d39a49944e5b4fc49eb010fe94ace54043696760
Prakash Shelke <avikdigidev@gmail.com>
2020-09-13 02:42:58 +0530

    Added Angular Service
    Product List not visible

---
+++ angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,16 @@
+export class Product {
+	id: number;
+	categoryId: number;
+	// private ProductCategory productCategory;
+	sku : string ;
+	name : string ;
+	description : string; 
+	unitPrice : number;
+	imageUrl: string ;
+	active: boolean;
+	unitsInStock: number ;
+dateCreated: Date;
+dateUpdated: Date;
+	//categoryName: string ;
+
+}
